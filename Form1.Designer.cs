bf5a91f883d1c14d60c3a20202f75c830fff5db7
TheAndreyyy <theandreyyy@gmail.com>
2020-12-26 14:18:59 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace BFAFv2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Стоп";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 151);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 20;
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 20;
+            this.timer3.Tick += new System.EventHandler(this.Timer3_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(174, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(116, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "забросили удочку";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(221, 35);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(57, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "клюёт";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(174, 58);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(104, 17);
+            this.checkBox3.TabIndex = 8;
+            this.checkBox3.Text = "идет мини-игра";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(174, 81);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(157, 17);
+            this.checkBox4.TabIndex = 9;
+            this.checkBox4.Text = "выиграли и забрали рыбу";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 20;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 50;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 173);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Bot For Albion Fishing v2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;//for working mouse and buttons
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace BFAFv2
+{
+
+    public partial class Form1 : Form
+    {
+        #region options_of_program
+        Stopwatch stopwatch = new Stopwatch();//debug for count
+        int state_of_fishing = -1;//состояние процесса рыбалки
+        /*
+         * 2 - мини-игра
+         */
+        Point pos_of_match = new Point(0, 0);
+        Point pos_of_match_poplavok = new Point(0, 0);
+        bool mouse_pressed_now = false;
+        int MaxPos_Of_Match_Y=0;
+        bool prev_state_fishing = false;
+        #endregion
+
+        #region OPENCV_options
+
+        //string path_for_files = "C:\\Users\\Andrey\\Desktop\\";
+        string path = "D:\\";
+
+        #endregion
+
+        #region mouse_options
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        #endregion
+
+        #region CV
+        public void Find_matches(string where, string what, float percent)
+        {
+            // Запускаем внутренний таймер объекта Stopwatch
+            stopwatch.Start();
+            Image<Bgr, byte> source = new Image<Bgr, byte>(path + where + ".bmp"); // Image A где ищем, картинка рыбалки
+            Image<Bgr, byte> template = new Image<Bgr, byte>(path + what + ".bmp"); // Image B что ищем
+            Image<Bgr, byte> imageToShow = source.Copy();
+            using (Image<Gray, float> result = source.MatchTemplate(template, Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed))
+            {
+                double[] minValues, maxValues;
+                Point[] minLocations, maxLocations;
+                result.MinMax(out minValues, out maxValues, out minLocations, out maxLocations);
+
+                // You can try different values of the threshold. I guess somewhere between 0.75 and 0.95 would be good.
+                if (maxValues[0] > percent)//0.9
+                {
+                    // This is a match. Do something with it, for example draw a rectangle around it.
+                    Rectangle match = new Rectangle(maxLocations[0], template.Size);
+                    imageToShow.Draw(match, new Bgr(Color.Red), 3);
+                    pos_of_match = maxLocations[0];
+                    label1.Text = "Позиция совпадения: X - " + pos_of_match.X + " | Y - " + pos_of_match.Y;
+                }
+                else
+                {
+                    if (what != "fish_is_mine")
+                    {
+                        label1.Text = "Совпадений не найдено";
+                    }
+                }
+                label2.Text = "Процент совпадения: " + Math.Round(maxValues[0], 3).ToString();
+            }
+            imageToShow.Save(path + "final.bmp");
+            stopwatch.Stop();// Останавливаем внутренний таймер объекта Stopwatch
+            label4.Text = "Время опознания: " + (stopwatch.ElapsedMilliseconds).ToString() + " мс";//time of exec CV
+            stopwatch.Reset();
+        }
+        #endregion
+
+        public Form1()
+        {
+            TopMost = true;
+            InitializeComponent();
+        }
+
+        public void MyMouseMove(int x, int y)
+        {
+            PointConverter pc = new PointConverter();
+            System.Drawing.Point pt = new System.Drawing.Point(x, y);
+            Cursor.Position = pt;
+        }
+
+        public void Mouse_press(int state)
+        {
+            if (state == 0)
+            {
+                //Call the imported function with the cursor's current position
+                uint X = (uint)Cursor.Position.X;
+                uint Y = (uint)Cursor.Position.Y;
+                mouse_event(MOUSEEVENTF_LEFTDOWN, X, Y, 0, 0);
+                //Thread.Sleep(time); 
+                //mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+                mouse_pressed_now = true;
+            }
+            else
+            if (state == 1)
+            {
+                //Call the imported function with the cursor's current position
+                uint X = (uint)Cursor.Position.X;
+                uint Y = (uint)Cursor.Position.Y;
+                mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+                mouse_pressed_now = false;
+            }
+
+        }
+
+        private void Get_screenshot(int x1, int y1, int x2, int y2)
+        {
+            Bitmap printscreen = new Bitmap(x2 - x1, y2 - y1/*Screen.PrimaryScreen.Bounds.Height*/);
+
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+
+            graphics.CopyFromScreen(x1, y1, 0, 0, printscreen.Size);
+
+            printscreen.Save(path + "screenshot.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+            //Close();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (state_of_fishing == 1)
+            {
+
+            }
+            else
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            if (state_of_fishing == 2)
+            {
+
+            }
+            else
+            {
+                timer2.Enabled = false;
+            }
+        }
+
+        private void Timer3_Tick(object sender, EventArgs e)
+        {
+            if (state_of_fishing == 3)
+            {
+
+            }
+            else
+            {
+                timer3.Enabled = false;
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (state_of_fishing == 4)
+            {
+
+            }
+            else
+            {
+                timer4.Enabled = false;
+            }
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            pos_of_match.X = 0;
+            pos_of_match.Y = 0;
+
+            if (state_of_fishing >= 1)
+            {
+                #region 1poplavok
+                Get_screenshot(1010, 130, 1120, 220);                   //скрин поплавка - есть ли он вообще
+                Find_matches("screenshot", "poplavok_new", 0.5f);
+                //Close();
+                if (pos_of_match.X > 0)                                 //если нашел поплавок
+                {
+                    if (pos_of_match.Y>MaxPos_Of_Match_Y)//если сейчас поплавок ниже, то записываем это значение в "максимальное"
+                    {
+                        MaxPos_Of_Match_Y = pos_of_match.Y;
+                        label5.Text = MaxPos_Of_Match_Y.ToString();
+
+                    }
+                    pos_of_match_poplavok.X = pos_of_match.X;           //найден поплавок
+                    if (checkBox1.Checked == false)
+                    {
+                        checkBox1.Checked = true;                       //зажигаем лампочку
+                    }
+                    //state_of_fishing = 1;
+                    prev_state_fishing = true;//мы только что ловили рыбу
+
+                }
+                else//иначе если не нашел поплавок
+                {
+                    if (checkBox1.Checked == true)
+                    {
+                        checkBox1.Checked = false;                      //тушим лампочку
+                    }
+                    pos_of_match_poplavok.X = 0;                        //поплавок мы не нашли
+                    prev_state_fishing = false;//мы не ловили рыбу
+                }
+                pos_of_match.X = 0;
+                #endregion
+
+                #region 2klev
+                Get_screenshot(1010, 130, 1120, 220);                   //скрин клёва
+                Find_matches("screenshot", "poplavok_new", 0.7f);
+                //if (pos_of_match.Y > 25 /*&& pos_of_match_poplavok.X == 0 && prev_state_fishing == true*/) //если точно не увидел в прошлую проверку и точно увидел сейчас, значит ушел под воду!!!
+                //{
+                if (MaxPos_Of_Match_Y - pos_of_match.Y > 13)
+                {
+                    
+                    if (checkBox2.Checked == false)
+                    {
+                        checkBox2.Checked = true;
+                    }
+                    state_of_fishing = 1;
+                    prev_state_fishing = false;
+                }
+                else
+                {
+                    if (checkBox2.Checked == true)
+                    {
+                        checkBox2.Checked = false;
+                    }
+                }
+                pos_of_match.X = 0;
+                #endregion
+
+                #region 3mini-game
+                Get_screenshot(850, 520, 1070, 570);//скрин мини-игры
+                Find_matches("screenshot", "mini-game", 0.7f);
+                //Close();
+                if (pos_of_match.X > 0)
+                {
+                    if (checkBox3.Checked == false)
+                    {
+                        checkBox3.Checked = true;
+                    }
+                    state_of_fishing = 1;
+                }
+                else
+                {
+                    if (checkBox3.Checked == true)
+                    {
+                        checkBox3.Checked = false;
+                    }
+                }
+                pos_of_match.X = 0;
+                #endregion
+
+                #region 4finish
+                Get_screenshot(767, 66, 1151, 88);//скрин выигрыша
+                Find_matches("screenshot", "fish_is_mine", 0.7f);
+                //Close();
+                if (pos_of_match.X > 0)
+                {
+                    if (checkBox4.Checked == false)
+                    {
+                        checkBox4.Checked = true;
+                    }
+                    state_of_fishing = 1;
+                }
+                else
+                {
+                    if (checkBox4.Checked == true)
+                    {
+                        checkBox4.Checked = false;
+                    }
+                }
+                pos_of_match.X = 0;
+                #endregion
+            }
+            else
+            {
+                timer5.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Start_fishing();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Stop_fishing();
+        }
+
+        private void Start_fishing()
+        {
+            state_of_fishing = 5;//fishing_state        start
+            timer5.Enabled = true;//fishing process     start 
+            button1.Enabled = false;//button start      false
+            button2.Enabled = true;//button stop        true
+        }
+
+        private void Stop_fishing()
+        {
+            MaxPos_Of_Match_Y = 0;
+            timer5.Enabled = false;
+            //timer1.Enabled = false;//cursor pos         stop
+            state_of_fishing = 0;//proc of fish        stop
+            button1.Enabled = true;//but start          enabled
+            button2.Enabled = false;//but stop          disable
+            //timer3.Enabled = false;//timer of fishing   stop
+            label6.Text = "Ждем...";
+        }
+    }
+}
